46e01d6400e3d1847fbfcc811782aa1508b1894a
s8189882 <s8189882@gmail.com>
2022-09-21 19:55:13 +0530

    create master branch

---
+++ Lambda Expression/src/lambdaexpression/mathoperations/MathOperationApp.java
@@ -0,0 +1,9 @@
+package lambdaexpression.mathoperations;
+
+public class MathOperationApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Math Operation App!\n");
+	}
+
+}
