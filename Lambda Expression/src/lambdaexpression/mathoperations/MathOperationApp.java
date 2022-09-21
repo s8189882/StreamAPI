842a81eb14415bd4747225decca5be524119cc77
s8189882 <s8189882@gmail.com>
2022-09-21 20:03:41 +0530

    Add UC1 - Develop Math Operation App to perform Math Functions Addition, Subtraction and Division

--- Lambda Expression/src/lambdaexpression/mathoperations/MathOperationApp.java
+++ Lambda Expression/src/lambdaexpression/mathoperations/MathOperationApp.java
@@ -1,9 +1,24 @@
 package lambdaexpression.mathoperations;
 
+
+@FunctionalInterface
+interface IMathFunction {
+	int calculate(int a, int b);
+}
+
 public class MathOperationApp {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Math Operation App!\n");
+		
+		IMathFunction add = (int x, int y) -> x + y;
+		IMathFunction subtract = (int x, int y) -> x - y;
+		IMathFunction multiply = (x, y) -> x * y;
+		IMathFunction divide = (int x, int y) -> x / y;
+		
+		System.out.println("Addition is " + add.calculate(25, 5));
+		System.out.println("Subtraction is " + subtract.calculate(25, 5));
+		System.out.println("Multiplication is " + multiply.calculate(25, 5));
+		System.out.println("Division is " + divide.calculate(25, 5));
 	}
-
 }
